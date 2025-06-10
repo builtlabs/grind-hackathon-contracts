53e4ec69b3b446fe7b9dbcf2ee4fa05e06d25d88
builtbyfrancis@outlook.com <builtbyfrancis@outlook.com>
2025-06-10 12:37:07 +0100

    Massively simplify paymaster

--- contracts/auxiliary/GeneralPaymaster.sol
+++ contracts/auxiliary/GeneralPaymaster.sol
@@ -8,26 +8,11 @@ import { BOOTLOADER_ADDRESS, Transaction } from "@matterlabs/zksync-contracts/co
 
 /// @title GeneralPaymaster
 /// @author BuiltByFrancis
-/// @notice A general paymaster implementing the ZK IPaymaster, with basic transaction validation.
+/// @notice A general "allow all" paymaster implementing the ZK IPaymaster interface.
 contract GeneralPaymaster is IPaymaster, Ownable {
     error NotBootloader();
-    error BlockedSender();
-    error BlockedDestination();
     error InvalidPaymasterInput();
     error FailedToTransferEther();
-
-    event FromBlockedSet(address indexed from, bool blocked);
-    event ToAllowedSet(address indexed to, bool allowed);
-
-    // #######################################################################################
-
-    /// @dev By default, all senders are allowed.
-    mapping(address => bool) private _fromBlocked;
-
-    // TODO: By default, allow all destinations.
-
-    /// @dev By default, all destinations are blocked.
-    mapping(address => bool) private _toAllowed;
 
     // #######################################################################################
 
@@ -39,41 +24,10 @@ contract GeneralPaymaster is IPaymaster, Ownable {
     // #######################################################################################
 
     /// @notice Constructor initializes the contract with the given parameters.
-    /// @param allowedTargets_ The initial allow list for transaction.to values.
     /// @param owner_ The owner of the contract.
-    constructor(address[] memory allowedTargets_, address owner_) Ownable(owner_) {
-        for (uint256 i = 0; i < allowedTargets_.length; i++) {
-            _setAllowedTo(allowedTargets_[i], true);
-        }
-    }
+    constructor(address owner_) Ownable(owner_) {}
 
     // #######################################################################################
-
-    /// @notice Returns whether a from address will be blocked.
-    function fromAddressIsBlocked(address _from) external view returns (bool) {
-        return _fromBlocked[_from];
-    }
-
-    /// @notice Returns whether a to address is allowed.
-    function toAddressIsAllowed(address _to) external view returns (bool) {
-        return _toAllowed[_to];
-    }
-
-    // #######################################################################################
-
-    /// @notice Sets the allowed status for a specific destination address.
-    /// @param _to The destination address to set the allowed status for.
-    /// @param _allowed The allowed status to set for the destination address.
-    function setAllowedTo(address _to, bool _allowed) external onlyOwner {
-        _setAllowedTo(_to, _allowed);
-    }
-
-    /// @notice Sets the blocked status for a specific sender address.
-    /// @param _from The sender address to set the blocked status for.
-    /// @param _blocked The blocked status to set for the sender address.
-    function setBlockedFrom(address _from, bool _blocked) external onlyOwner {
-        _setBlockedFrom(_from, _blocked);
-    }
 
     /// @notice Withdraws a specified amount of Ether from the contract to the owner's address.
     /// @param _amount The amount of Ether to withdraw.
@@ -83,8 +37,7 @@ contract GeneralPaymaster is IPaymaster, Ownable {
 
     /// @notice Withdraws all Ether from the contract to the owner's address.
     function withdrawAll() external onlyOwner {
-        uint256 balance = address(this).balance;
-        _sendEther(payable(owner()), balance);
+        _sendEther(payable(owner()), address(this).balance);
     }
 
     // #######################################################################################
@@ -97,14 +50,6 @@ contract GeneralPaymaster is IPaymaster, Ownable {
     ) external payable onlyBootloader returns (bytes4 magic, bytes memory context) {
         magic = PAYMASTER_VALIDATION_SUCCESS_MAGIC;
         context = new bytes(0); // No context is needed for this paymaster.
-
-        if (_fromBlocked[address(uint160(_transaction.from))]) {
-            revert BlockedSender();
-        }
-
-        if (!_toAllowed[address(uint160(_transaction.to))]) {
-            revert BlockedDestination();
-        }
 
         if (
             _transaction.paymasterInput.length < 4 ||
@@ -131,16 +76,6 @@ contract GeneralPaymaster is IPaymaster, Ownable {
     receive() external payable {}
 
     // #######################################################################################
-
-    function _setAllowedTo(address _to, bool _allowed) private {
-        _toAllowed[_to] = _allowed;
-        emit ToAllowedSet(_to, _allowed);
-    }
-
-    function _setBlockedFrom(address _from, bool _blocked) private {
-        _fromBlocked[_from] = _blocked;
-        emit FromBlockedSet(_from, _blocked);
-    }
 
     function _sendEther(address payable _to, uint256 _amount) private {
         (bool success, ) = _to.call{ value: _amount }("");
--- test/auxiliary/GeneralPaymaster.ts
+++ test/auxiliary/GeneralPaymaster.ts
@@ -36,7 +36,7 @@ function createDummyTransaction(
 
 describe("GeneralPaymaster", function () {
     async function fixture() {
-        const [deployer, targetA, targetB, blockedSender, blockedTarget, bank] = await ethers.getSigners();
+        const [deployer, targetA, targetB, bank] = await ethers.getSigners();
 
         await hre.network.provider.request({
             method: "hardhat_impersonateAccount",
@@ -50,10 +50,8 @@ describe("GeneralPaymaster", function () {
         await blocking.waitForDeployment();
 
         const SUT = await ethers.getContractFactory("GeneralPaymaster");
-        const sut = await SUT.deploy([targetA, targetB], deployer.address);
+        const sut = await SUT.deploy(deployer.address);
         await sut.waitForDeployment();
-
-        await sut.setBlockedFrom(blockedSender.address, true);
 
         await bank.sendTransaction({
             to: sut.target,
@@ -73,8 +71,6 @@ describe("GeneralPaymaster", function () {
                 bootloader,
                 targetA,
                 targetB,
-                blockedSender,
-                blockedTarget,
             },
         };
     }
@@ -86,101 +82,6 @@ describe("GeneralPaymaster", function () {
             const { sut, wallets } = await loadFixture(fixture);
 
             expect(await sut.owner()).to.equal(wallets.deployer.address);
-        });
-
-        it("Should set the allowed targets", async function () {
-            const { sut, wallets } = await loadFixture(fixture);
-
-            expect(await sut.toAddressIsAllowed(wallets.targetA)).to.equal(true);
-            expect(await sut.toAddressIsAllowed(wallets.targetB)).to.equal(true);
-        });
-
-        it("Should emit ToAllowedSet", async function () {
-            const { wallets } = await loadFixture(fixture);
-
-            const SUT = await ethers.getContractFactory("GeneralPaymaster");
-            const sut = await SUT.deploy([wallets.targetA.address, wallets.targetB.address], wallets.deployer.address);
-            const receipt = (await sut.deploymentTransaction()!.wait())!;
-
-            const iface = SUT.interface;
-
-            const topic = id("ToAllowedSet(address,bool)");
-            const referralEvents = receipt.logs
-                .filter((log) => log.topics[0] === topic)
-                .map((log) => iface.decodeEventLog("ToAllowedSet", log.data, log.topics));
-
-            expect(referralEvents).to.deep.include.members([
-                [wallets.targetA.address, true],
-                [wallets.targetB.address, true],
-            ]);
-        });
-    });
-
-    describe("setAllowedTo", function () {
-        it("Should revert if the caller is not the owner", async function () {
-            const { sut, wallets } = await loadFixture(fixture);
-
-            const target = wallets.targetA;
-            const old = await sut.toAddressIsAllowed(target.address);
-
-            await expect(sut.connect(target).setAllowedTo(target.address, !old))
-                .to.be.revertedWithCustomError(sut, "OwnableUnauthorizedAccount")
-                .withArgs(target.address);
-        });
-
-        it("Should set allowed to", async function () {
-            const { sut, wallets } = await loadFixture(fixture);
-
-            const target = wallets.targetA;
-            const old = await sut.toAddressIsAllowed(target.address);
-
-            await sut.connect(wallets.deployer).setAllowedTo(target.address, !old);
-            expect(await sut.toAddressIsAllowed(target.address)).to.equal(!old);
-        });
-
-        it("Should emit ToAllowedSet", async function () {
-            const { sut, wallets } = await loadFixture(fixture);
-
-            const target = wallets.targetA;
-            const old = await sut.toAddressIsAllowed(target.address);
-
-            await expect(sut.connect(wallets.deployer).setAllowedTo(target.address, !old))
-                .to.emit(sut, "ToAllowedSet")
-                .withArgs(target.address, !old);
-        });
-    });
-
-    describe("setBlockedFrom", function () {
-        it("Should revert if the caller is not the owner", async function () {
-            const { sut, wallets } = await loadFixture(fixture);
-
-            const target = wallets.targetA;
-            const old = await sut.fromAddressIsBlocked(target.address);
-
-            await expect(sut.connect(target).setBlockedFrom(target.address, !old))
-                .to.be.revertedWithCustomError(sut, "OwnableUnauthorizedAccount")
-                .withArgs(target.address);
-        });
-
-        it("Should set blocked from", async function () {
-            const { sut, wallets } = await loadFixture(fixture);
-
-            const target = wallets.targetA;
-            const old = await sut.fromAddressIsBlocked(target.address);
-
-            await sut.connect(wallets.deployer).setBlockedFrom(target.address, !old);
-            expect(await sut.fromAddressIsBlocked(target.address)).to.equal(!old);
-        });
-
-        it("Should emit FromBlockedSet", async function () {
-            const { sut, wallets } = await loadFixture(fixture);
-
-            const target = wallets.targetA;
-            const old = await sut.fromAddressIsBlocked(target.address);
-
-            await expect(sut.connect(wallets.deployer).setBlockedFrom(target.address, !old))
-                .to.emit(sut, "FromBlockedSet")
-                .withArgs(target.address, !old);
         });
     });
 
@@ -283,30 +184,6 @@ describe("GeneralPaymaster", function () {
             await expect(
                 sut.connect(wallets.targetA).validateAndPayForPaymasterTransaction(hash, suggestedHash, transaction)
             ).to.be.revertedWithCustomError(sut, "NotBootloader");
-        });
-
-        it("Should revert if the from address is blocked", async function () {
-            const { sut, wallets } = await loadFixture(fixture);
-
-            const hash = hexlify(randomBytes(32));
-            const suggestedHash = hexlify(randomBytes(32));
-            const transaction = createDummyTransaction(wallets.blockedSender.address, wallets.targetB.address);
-
-            await expect(
-                sut.connect(wallets.bootloader).validateAndPayForPaymasterTransaction(hash, suggestedHash, transaction)
-            ).to.be.revertedWithCustomError(sut, "BlockedSender");
-        });
-
-        it("Should revert if the to address is not allowed", async function () {
-            const { sut, wallets } = await loadFixture(fixture);
-
-            const hash = hexlify(randomBytes(32));
-            const suggestedHash = hexlify(randomBytes(32));
-            const transaction = createDummyTransaction(wallets.targetA.address, wallets.blockedTarget.address);
-
-            await expect(
-                sut.connect(wallets.bootloader).validateAndPayForPaymasterTransaction(hash, suggestedHash, transaction)
-            ).to.be.revertedWithCustomError(sut, "BlockedDestination");
         });
 
         it("Should revert if the paymaster input is too short", async function () {
